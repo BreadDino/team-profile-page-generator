82133e939d19d75765352215ad777955663aec8b
unknown <gabemarredondo@gmail.com>
2022-09-15 16:33:46 -0700

    added user questions array and employee card makers

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateHTML = require("./src/generateHTML")
+const questions = [
+    {
+       
+    },
+    {
+       
+    }
+]
 
+// function to create html file for team
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err)=>
+        (err) ? console.log("error") : console.log("HTML File Generated")
+    )
+}
+
+// function to use inquirer to get user input at start
+function init() {
+    inquirer.prompt (questions)
+    .then((answers)=>{
+        const htmlOutput = generateHTML(answers)
+        writeToFile("team.html", htmlOutput)
+    })
+}
+
+// Function call to initialize app
+init();
