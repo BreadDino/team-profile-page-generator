d6045b002e3a54cc31d1622403d6354544fecfeb
unknown <gabemarredondo@gmail.com>
2022-09-18 12:58:49 -0700

    added beginning questions and questions for adding additional employees aswell as functions to write to generateHTML.js

--- index.js
+++ index.js
@@ -1,30 +1,91 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHTML = require("./src/generateHTML")
+const renderEmployeeCards = require("./src/generateHTML")[0];
+
+const employeeArray = [];
+
 const questions = [
-    {
-       
+  {
+    type: "input",
+    name: "name",
+    message: "what is your name?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "what is your employee id?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is your email?",
+    validate: function (input) {
+      {
+        validEmail =
+          /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()\.,;\s@\"]+\.{0,1})+([^<>()\.,;:\s@\"]{2,}|[\d\.]+))$/.test(
+            input
+          );
+
+        if (validEmail) {
+          return true;
+        } else {
+          console.log(" Invalid email address");
+          return false;
+        }
+      }
     },
-    {
-       
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "what is your office number?",
+  },
+  {
+    type: "list",
+    name: "typeEmployee",
+    message: "what type of employee would you like to add?",
+    choices: ["Engineer", "Intern", "No More Employees"],
+  },
+];
+
+async function init() {
+  let questioning = true;
+
+  while (questioning) {
+    const answers = await inquirer.prompt(questions);
+
+    if (answers.typeEmployee === "No More Employees") {
+      questioning = false;
     }
-]
 
-// function to create html file for team
+    if (answers.typeEmployee === "Engineer") {
+      questions.splice(3, 1, {
+        type: "input",
+        name: "github",
+        message: "what is your github username?",
+      });
+    }
+
+    if (answers.typeEmployee === "Intern") {
+      questions.splice(3, 1, {
+        type: "input",
+        name: "school",
+        message: "what is the name of your school?",
+      });
+    }
+
+    employeeArray.push(answers);
+  }
+
+  const htmlOutput = renderEmployeeCards(employeeArray);
+  writeToFile("./dist/team.html", htmlOutput);
+}
+
+// creates html file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err)=>
-        (err) ? console.log("error") : console.log("HTML File Generated")
-    )
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.log("error") : console.log("HTML File Generated")
+  );
 }
-
-// function to use inquirer to get user input at start
-function init() {
-    inquirer.prompt (questions)
-    .then((answers)=>{
-        const htmlOutput = generateHTML(answers)
-        writeToFile("team.html", htmlOutput)
-    })
-}
-
-// Function call to initialize app
+// starts the application
 init();
